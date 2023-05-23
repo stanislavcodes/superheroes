aab076b2be1385e916c02552d7c28f65318d82dd
Stanislav Korchevskyi <stanislav.codes@gmail.com>
2023-05-23 21:15:44 +0300

    fixed error in alias

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   resolve: {
     alias: {
-      '~/': resolve(__dirname, './src/'),
+      '~': resolve(__dirname, './src/'),
     },
   },
   plugins: [react()],
