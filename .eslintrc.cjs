8a90e89b51cbda33c9565c7666b4d928ebdaf9c6
Stanislav Korchevskyi <stanislav.codes@gmail.com>
2023-05-23 19:13:53 +0300

    configurations

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,8 +7,20 @@ module.exports = {
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'react-hooks'],
   rules: {
+    'react-hooks/rules-of-hooks': 'error',
     'react-refresh/only-export-components': 'warn',
+    'react/prop-types': 0,
+    'react/self-closing-comp': 0,
+    'react/display-name': 0,
+    semi: 0,
+    'no-proto': 0,
+    'no-unused-vars': 0,
+    '@typescript-eslint/no-unused-vars': ['error'],
+    '@typescript-eslint/camelcase': 0,
+    '@typescript-eslint/explicit-module-boundary-types': 0,
+    '@typescript-eslint/ban-types': 0,
+    '@typescript-eslint/semi': ['error'],
   },
-}
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+      '@styles': resolve(__dirname, './src/styles'),
+      '@components': resolve(__dirname, './src/components'),
+      '@assets': resolve(__dirname, './src/assets'),
+      '@api': resolve(__dirname, './src/api'),
+      '@pages': resolve(__dirname, './src/pages'),
+      '@layouts': resolve(__dirname, './src/layouts'),
+      '@utils': resolve(__dirname, './src/utils'),
+      '@fonts': resolve(__dirname, './src/fonts'),
+      '@hooks': resolve(__dirname, './src/hooks'),
+      '@contexts': resolve(__dirname, './src/contexts'),
+    },
+  },
   plugins: [react()],
 })
