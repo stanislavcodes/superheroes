ddd47c306a583a8c5fda528b124f79417ec5fe29
Stanislav Korchevskyi <stanislav.codes@gmail.com>
2023-05-23 21:01:09 +0300

    updated aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,13 @@
-import { defineConfig } from 'vite'
-import { resolve } from 'path'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    resolve: {
+  resolve: {
     alias: {
-      '@': resolve(__dirname, './src'),
-      '@styles': resolve(__dirname, './src/styles'),
-      '@components': resolve(__dirname, './src/components'),
-      '@assets': resolve(__dirname, './src/assets'),
-      '@api': resolve(__dirname, './src/api'),
-      '@pages': resolve(__dirname, './src/pages'),
-      '@layouts': resolve(__dirname, './src/layouts'),
-      '@utils': resolve(__dirname, './src/utils'),
-      '@fonts': resolve(__dirname, './src/fonts'),
-      '@hooks': resolve(__dirname, './src/hooks'),
-      '@contexts': resolve(__dirname, './src/contexts'),
+      '~/': resolve(__dirname, './src/'),
     },
   },
   plugins: [react()],
-})
+});
