c5b6a1e88f03a32378e7cfb15b486900edf2819c
Stanislav Korchevskyi <stanislav.codes@gmail.com>
2023-05-23 21:14:49 +0300

    added Superhero type

---
+++ src/types/Superhero.ts
@@ -0,0 +1,9 @@
+export interface Superhero {
+  id: string;
+  nickname: string;
+  real_name: string;
+  origin_description: string;
+  superpowers: string;
+  catch_phrase: string;
+  images: string[];
+}
